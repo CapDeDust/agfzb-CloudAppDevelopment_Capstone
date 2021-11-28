4bb50661487c86677138c04a0401385b51f1fe68
CapDeDust <92262566+CapDeDust@users.noreply.github.com>
2021-11-28 14:14:59 -0800

    Changing hostname

--- server/djangobackend/settings.py
+++ server/djangobackend/settings.py
@@ -27,7 +27,7 @@ DEBUG = True
 
 APPEND_SLASH = True
 
-ALLOWED_HOSTS = ["localhost", "42randomdust24.mybluemix.net"]
+ALLOWED_HOSTS = ["localhost", "424242randomDust242424.mybluemix.net"]
 
 
 # Application definition
