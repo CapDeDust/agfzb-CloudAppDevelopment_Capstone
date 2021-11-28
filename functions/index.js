b7e04fd9d9430469f2b35df6320bdbaa889c79c6
CapDeDust <92262566+CapDeDust@users.noreply.github.com>
2021-11-28 07:39:06 -0800

    Adjustments based on instructor feedback

--- functions/index.js
+++ functions/index.js
@@ -1,57 +1,90 @@
-/**
-  *
-  * main() will be run when you invoke this action
-  *
-  * @param Cloud Functions actions accept a single parameter, which must be a JSON object.
-  *
-  * @return The output of this action, which must be a JSON object.
-  *
-  */
- const Cloudant = require('@cloudant/cloudant');
-  
- async function main(params) {
-     const cloudant = Cloudant({
-         url: params.cloudant_url,
-         plugins: { iamauth: { iamApiKey: params.cloudant_apikey } }
-     });
- 
-     try {
-         let dbList = await cloudant.db.use("dealerships");
-         dbList.list({include_docs: true});
-         
-         let q;
-         let response;
-         if (params.state) {
-             q = {
-                 selector: {
-                 _id: { "$gt": "0"},
-                 st: {"$eq": params.state }
-                 },
-                 fields: [ "id", "city", "state", "st", "address", "zip", "lat", "long" ]
-             };
-             response = await dbList.find(q);
-             if (response.bookmark === 'nil') {
-                 return { 
-                     "headers": { 
-                         "Content-Type": "application/json" 
-                         
-                     }, 
-                     "statusCode": "404", 
-                     "error": "The state does not exist"
-                 }
-             }
-         } else {
-             q = {
-                 selector: {
-                 _id: { "$gt": "0"}
-                 },
-                 fields: [ "id", "city", "state", "st", "address", "zip", "lat", "long" ]
-             };
-             response = await dbList.find(q);
-         }
-         
-         return response;
-     } catch (error) {
-         return { error: error.description };
-     }    
- }
+function main(params) {
+  return new Promise(function (resolve, reject) {
+    const Cloudant = require("@cloudant/cloudant");
+
+    const cloudant = Cloudant({
+      url: params.cloudant_url,
+      plugins: { iamauth: { iamApiKey: params.cloudant_apikey } },
+    });
+
+    const dealershipDb = cloudant.use("dealerships");
+    if (params.state) {
+      // return dealership with this state
+      dealershipDb.find(
+        {
+            selector: {
+                _id: { "$gt": "0"},
+                st: { "$eq": params.state }
+            },
+            fields: [ "id", "city", "state", "st", "address", "zip", "lat", "long" ]
+        },
+        
+        function (err, result) {
+          if (err) {
+            reject(err);
+          }
+
+          let code = 200;
+
+          if (result.docs.length == 0) {
+            result.error = "No matching state found with that abbreviation";
+            code = 404;
+          }
+
+          resolve({
+            statusCode: code,
+            headers: { "Content-Type": "application/json" },
+            body: result,
+          });
+        }
+      );
+    } else if (params.id) {
+      id = parseInt(params.dealerId);
+      // return dealership with this state
+      dealershipDb.find(
+          {
+            selector: {
+                id: { "$eq": parseInt(params.id) }
+            },
+            fields: [ "id", "city", "state", "st", "address", "zip", "lat", "long" ]
+        },
+        
+        function (err, result) {
+          if (err) {
+            reject(err);
+          }
+
+          let code = 200;
+
+          if (result.docs.length == 0) {
+            code = 404;
+            result.error = "No matching state found with that id";
+          }
+          resolve({
+            statusCode: code,
+            headers: { "Content-Type": "application/json" },
+            body: result,
+          });
+        }
+      );
+    } else {
+      // return all documents
+      dealershipDb.find({ 
+          selector: {
+            _id: { "$gt": "0"}
+          },
+          fields: [ "id", "city", "state", "st", "address", "zip", "lat", "long" ]
+      }, function (err, result) {
+        if (err) {
+          reject(err);
+        }
+
+        resolve({
+          statusCode: 200,
+          headers: { "Content-Type": "application/json" },
+          body: result,
+        });
+      });
+    }
+  });
+}
