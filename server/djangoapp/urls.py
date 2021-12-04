8092ea4c3b8361d3eac65ad116505b030988871e
CapDeDust <92262566+CapDeDust@users.noreply.github.com>
2021-12-04 10:32:08 -0800

    Update urls.py

--- server/djangoapp/urls.py
+++ server/djangoapp/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from django.conf.urls.static import static
 from django.conf import settings
+# from django.conf.urls import patterns, url
+from django.views.decorators.csrf import csrf_exempt
 from . import views
 
 app_name = 'djangoapp'
@@ -24,10 +26,14 @@ urlpatterns = [
     # path for logout
     path(route='logout', view=views.logout_request, name='logout'),
 
+    # path(route='', view=views.get_dealerships_by_id, name='index'),
     path(route='', view=views.get_dealerships, name='index'),
 
     # path for dealer reviews view
+    path('dealer/<int:dealer_id>/', views.get_dealer_details, name='dealer_details'),
 
     # path for add a review view
+    # path('add_review/2', views.add_review, name='add_review')
+    path('dealer/<int:dealer_id>/', views.add_review, name='add_review')
 
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
